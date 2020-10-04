1f753fcdf8bf50a4a7339c21282354356fa78147
sreeraghavan <sreeraghavan.off@gmail.com>
2020-10-04 22:52:59 +0530

    Add files via upload

---
+++ diamond_star.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n, c, k;
+  clrscr();
+  printf("Enter number of rows\n");
+  scanf("%d", &n);
+
+  for (k = 1; k <= n; k++)
+  {
+    for (c = 1; c <= n-k; c++)
+      printf(" ");
+
+    for (c = 1; c <= 2*k-1; c++)
+      printf("*");
+
+    printf("\n");
+  }
+
+  for (k = 1; k <= n - 1; k++)
+  {
+    for (c = 1; c <= k; c++)
+      printf(" ");
+
+    for (c = 1 ; c <= 2*(n-k)-1; c++)
+      printf("*");
+
+    printf("\n");
+  }
+
+  getch();
+}
